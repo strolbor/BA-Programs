5b31ea11ff042c43d977a202c3d3fc8060c00751
Urs Braun <github@ursb.de>
2024-04-29 18:17:05 +0200

    jeweils 10 graphen in diagreamm

--- plot-group.py
+++ plot-group.py
@@ -10,7 +10,7 @@ def create_folder_if_not_exists(folder_path):
         os.makedirs(folder_path)
 
 # Funktion zum Plotten der Daten für die neuesten zehn Solver
-def plot_latest_solvers(data_dict):
+def plot_latest_solvers(data_dict,piccounter):
     plt.figure(figsize=(10, 6))  # Größe des Diagramms festlegen
     for solver, data in data_dict.items():
         versions = []
@@ -27,17 +27,21 @@ def plot_latest_solvers(data_dict):
         # Plotte die Daten und benenne den Graphen entsprechend dem Solver
         plt.plot(versions_sorted, times_sorted, marker='o', label=solver)
 
+    print()
     plt.xlabel('Version')
     plt.ylabel('Benötigte Zeit (ns)')
-    plt.title('Ausführungszeit pro Version für die neuesten zehn Solver')
+    plt.title('Ausführungszeit pro Version für zehn Solver')
     plt.xticks(rotation=45*2)
     plt.grid(True)
     plt.tight_layout()
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Legende außerhalb des Diagramms platzieren
     picord = "pics-solvers"
     create_folder_if_not_exists(picord)
-    plt.savefig(os.path.join(picord, 'latest_solvers_plot.png'), bbox_inches='tight')  # bbox_inches='tight' hinzugefügt
+    plt.savefig(os.path.join(picord, f'latest_solvers_plot-{piccounter}.png'), bbox_inches='tight')  # bbox_inches='tight' hinzugefügt
     print("saved pic")
+    plt.clf()
+
+
 
 # Hauptfunktion
 def main():
@@ -52,19 +56,20 @@ def main():
     # Extrahiere das Jahr aus dem Dateinamen
     for folder in folders:
         year = folder.split('_')[2][:2]  # Extrahiere die ersten beiden Zeichen des zweiten Teils des Ordnernamens
-        print(year)
         solver = folder.split('_')[-1]  # Extrahiere den Namen des Solvers
         solver_years[folder] = (year, solver)
 
-    print(solver_years)
     # Sortiere die Ordnernamen nach ihrem Jahr
     folders_sorted = sorted(solver_years.keys(), key=lambda x: int(solver_years[x][0]))
 
     # Wähle die Daten der ersten zehn Solver aus
-    latest_folders = folders_sorted[-10:]
+    #latest_folders = folders_sorted[-10:]
 
-    for folder in latest_folders:
-        print(folder)
+
+    firstten = folders_sorted[0:10] # Anzahl 10
+    print("1",firstten)
+
+    for folder in firstten:
         csv_path = os.path.join(folder, 'output.csv')
         if os.path.exists(csv_path):
             with open(csv_path, 'r') as file:
@@ -75,8 +80,42 @@ def main():
                 data_dict[folder] = data
 
     # Plotte die Daten für die neuesten zehn Solver
-    plot_latest_solvers(data_dict)
+    plot_latest_solvers(data_dict,'0')
 
+    data_dict = {}
+    #secound = tmp.copy()
+    secound = folders_sorted[10:20] # Anzahl 10 
+    print("2:" ,secound)
+    for folder in secound:
+        csv_path = os.path.join(folder, 'output.csv')
+        if os.path.exists(csv_path):
+            with open(csv_path, 'r') as file:
+                csv_reader = csv.DictReader(file, delimiter=',')
+                next(csv_reader) 
+                data = list(csv_reader)
+                # Füge die Daten dem entsprechenden Solver im Dictionary hinzu
+                data_dict[folder] = data
+
+    # Plotte die Daten für die neuesten zehn Solver
+    plot_latest_solvers(data_dict,'1')
+
+    data_dict = {}
+    #secound = tmp.copy()
+    third = folders_sorted[20:30]
+    print("3:" ,third)
+
+    for folder in third:
+        csv_path = os.path.join(folder, 'output.csv')
+        if os.path.exists(csv_path):
+            with open(csv_path, 'r') as file:
+                csv_reader = csv.DictReader(file, delimiter=',')
+                next(csv_reader) 
+                data = list(csv_reader)
+                # Füge die Daten dem entsprechenden Solver im Dictionary hinzu
+                data_dict[folder] = data
+
+    # Plotte die Daten für die neuesten zehn Solver
+    plot_latest_solvers(data_dict,'2')
 
 
 
