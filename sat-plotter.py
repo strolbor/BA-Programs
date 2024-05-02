6ca4fd14de7cdfc502e765c2fa3242c769e5b6ff
Urs Braun <github@ursb.de>
2024-05-02 09:23:16 +0200

    Changes to be committed: 	modified:   sat-plotter.py     new file:   test.py

--- sat-plotter.py
+++ sat-plotter.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 # Kurzbeschreibung
-# Es wird der Graph über die benötigte ZEit für alle Feature Modell mit dem entsprechenden Solver geplottet.
+# Es wird der Graph über die benötigte Zeit für alle Feature Modelle mit dem entsprechenden Solver geplottet.
 
 
 # Ordner erstellen, wenn er nicht existiert
@@ -15,13 +15,13 @@ def create_folder_if_not_exists(folder_path):
 
 
 # Funktion zum Plotten der Daten für alle Solver
-def plot_all_solvers(data_dict,dateiname):
+def plotter_fkt(data_dict, dateiname,title):
     plt.figure(figsize=(10, 6))  # Größe des Diagramms festlegen
-    for solver, data in data_dict:
+    for solver, data in data_dict.items():
         versions = []
         times = []
         for row in data:
-            versions.append(row['dimacs-file'].split('/')[-1].split('[')[0])
+            versions.append(row['dimacs-analyzer'].split('/')[-1].split('[')[0])
             times.append(int(row['dimacs-analyzer-time']))
 
         # Sortiere die Versionen nach ihrem numerischen Wert
@@ -34,47 +34,74 @@ def plot_all_solvers(data_dict,dateiname):
 
     plt.xlabel('Version')
     plt.ylabel('Benötigte Zeit (ns)')
-    plt.title('Ausführungszeit pro Version für verschiedene Solver')
+    plt.title(title)
     plt.xticks(rotation=45*2)
     plt.grid(True)
     plt.tight_layout()
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))  # Legende außerhalb des Diagramms platzieren
-    print(os.path.join(f"{dateiname}-sa.png"))
-    plt.savefig(os.path.join(f"{dateiname}-sa.png"), bbox_inches='tight')  # bbox_inches='tight' hinzugefügt
-    #plt.show()
-
-
-# 'all_solvers_plot.png'
-
-
-# Filterfunktion definieren
-def filter_entries(entry):
-    return '[i386]' not in entry['dimacs-file']
+    plt.savefig(os.path.join(f"{dateiname}"), bbox_inches='tight')  # bbox_inches='tight' hinzugefügt
+    # plt.show()
 
 # Hauptfunktion
 def all_plotter():
     # Neuer Ordner mit allen Daten ist: sorted_by_SAT
-    #csvdaten = [datei for datei in os.listdir('sorted_by_SAT') if datei.endswith('-all.csv')]
+    # csvdaten = [datei for datei in os.listdir('sorted_by_SAT') if datei.endswith('-all.csv')]
     ordnerPath = 'sorted_by_SAT'
     dateien = os.listdir(ordnerPath)
     csvdaten = [datei for datei in dateien if datei.endswith('-all.csv')]
-    print(csvdaten)
-
 
     data_dict = {}
     for csvdatei in csvdaten:
-        path = os.path.join(ordnerPath,csvdatei)
-        with open(path,'r') as file:
-            
+        path = os.path.join(ordnerPath, csvdatei)
+        with open(path, 'r') as file:
             csv_reader = csv.DictReader(file, delimiter=',')
-            next(csv_reader) 
-            data_dict = list(csv_reader)
+            #print(f"Struktur der CSV-Datei {csvdatei}: {csv_reader.fieldnames}")
+            next(csv_reader)
+            data_dict[csvdatei] = list(csv_reader)
 
     # Plotte die Daten für alle Solver
-    data_dict = data_dict[0]
-    print("\n")
-    print(data_dict)
-    plot_all_solvers(data_dict,"test.pnng")
+    datei = os.path.join(ordnerPath,"sat-all-plot.png")
+    title = 'Ausführungszeit pro Version für verschiedene Solver'
+    plotter_fkt(data_dict, datei,title)
+
+# def separed_plotter():
+#     # Neuer Ordner mit allen Daten ist: sorted_by_SAT
+#     ordnerPath = 'sorted_by_SAT'
+#     dateien = os.listdir(ordnerPath)
+#     csvdaten = [datei for datei in dateien if datei.endswith('-all.csv')]
+
+#     for csvdatei in csvdaten:
+#         path = os.path.join(ordnerPath, csvdatei)
+#         with open(path, 'r') as file:
+#             csv_reader = csv.DictReader(file, delimiter=',')
+#             next(csv_reader)
+#             data = list(csv_reader)
+        
+#         solver = csvdatei.split('-')[0]
+#         dateiname = os.path.join(solver,"-plot.png")
+#         print(dateiname)
+#         plotter_fkt(data, dateiname)
+
+def separed_plotter():
+    # Neuer Ordner mit allen Daten ist: sorted_by_SAT
+    ordnerPath = 'sorted_by_SAT'
+    dateien = os.listdir(ordnerPath)
+    csvdaten = [datei for datei in dateien if datei.endswith('-all.csv')]
+
+    for csvdatei in csvdaten:
+        path = os.path.join(ordnerPath, csvdatei)
+        data_dict = {}
+        with open(path, 'r') as file:
+            csv_reader = csv.DictReader(file, delimiter=',')
+            next(csv_reader)
+            data_dict[csvdatei] = list(csv_reader)
+        
+        solver = csvdatei.split('_')[2] + ".png"
+        title = "Solver: " + solver
+        datei = os.path.join(ordnerPath,solver)
+        plotter_fkt(data_dict, datei,title)
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -82,6 +109,8 @@ if __name__ == "__main__":
         exit()
     if int(sys.argv[1]) == 1:
         all_plotter()
+    elif int(sys.argv[1]) == 2:
+        separed_plotter()
     else:
         print("unbekannter Modus")
-     #main()
+    # main()
