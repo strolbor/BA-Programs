c8a45adc92e34203e73e6018db8a9959d6a24465
Urs <57560299+ubb21@users.noreply.github.com>
2024-06-10 16:21:37 +0200

    joird: converter

--- converter.py
+++ converter.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import argparse
 
-def convert(name: str):
+def convert(name: str, suffix: str):
     columns = ["Dataname", "lin_pearson_corr", "expo_pearson_corr", "best-fittest"]
     df = pd.read_csv(name, usecols=columns)
-    df.to_latex(name.replace(".csv",".tex"),index=False)
+    
+    # Filter the DataFrame
+    if suffix == "kconfigreader":
+        filtered_df = df[~df["Dataname"].str.endswith("kmax-median.csv")]
+    else:
+        filtered_df = df[~df["Dataname"].str.endswith("kconfigreader-median.csv")]
+
+    output_name = f"{name.replace('.csv', '')}-{suffix}.tex"
+    filtered_df.to_latex(output_name, index=False)
 
 def main():
     parser = argparse.ArgumentParser(description="Convert a CSV file to LaTeX format.")
     parser.add_argument("name", type=str, help="The name of the CSV file to convert")
     
     args = parser.parse_args()
-    convert(args.name)
+    convert(args.name, "kconfigreader")
+    convert(args.name, "kmax")
 
 if __name__ == "__main__":
     main()
